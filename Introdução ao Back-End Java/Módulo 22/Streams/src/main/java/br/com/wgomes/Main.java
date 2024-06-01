a72729660a0dc500911f51c9ca9d37e0fa8eb1f0
Wesley Gomes <47258234+Wesley-Gomes@users.noreply.github.com>
2024-06-01 03:55:29 -0300

    Update Main.java

--- Introdução ao Back-End Java/Módulo 22/Streams/src/main/java/br/com/wgomes/Main.java
+++ Introdução ao Back-End Java/Módulo 22/Streams/src/main/java/br/com/wgomes/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final String expected_gender = "FEMININO";
+        final String expectedGender = "FEMININO";
         System.out.println("Digite o nome e gênero separados por traço(-), separe os registros com vírgula:");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String[] records = input.split(",");
         System.out.println("Pessoas do gênero feminino inseridas:");
         Arrays.stream(records)
-                .filter(record -> record.split("\\s*-\\s*")[1].trim().equalsIgnoreCase(expected_gender))
+                .filter(record -> record.split("\\s*-\\s*")[1].trim().equalsIgnoreCase(expectedGender))
                 .forEach((person) -> System.out.println(person.split("\\s*-\\s*")[0].trim()));
     }
 }
